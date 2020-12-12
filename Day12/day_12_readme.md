bbfa0f5d5d23e1ea527533cd528b5207cbb14523
AdroMine <adro.mine@outlook.com>
2020-12-12 17:18:04 +0530

    udpate day 12 to use complex plane

--- Day12/day_12_readme.md
+++ Day12/day_12_readme.md
@@ -8,6 +8,13 @@ Instructions to move ship and ship/waypoint. Direction plus number given, move o
 - **Part 2** - find manhattan distance of ship, instructions move ship and a waypoint relative to ship, Ship moves in direction of waypoint
 
 ### Solution summary 
-Keep track of east and north. West and South are negative of these. 
-Rotate left by x degrees is equivalent to rotating right by 360-x degrees.
-Keep track of current direction with a named vector
+~~Keep track of east and north. West and South are negative of these.
+Keep track of current direction with a named vector~~
+<br>Rotate left by x degrees is equivalent to rotating right by 360-x degrees.
+
+#### Learned new trick of using complex number plane!
+Use complex number plane where X axis is represented by the real part and Y-axis by the imaginary part. 
+<br>
+Multiplication by i results in a counterclockwise rotation by 90 degrees.
+
+Also combine part1 and part2 in one function.
--- Day12/solution.R
+++ Day12/solution.R
@@ -9,79 +9,43 @@ parse_input <- function(input){
     input <- as.data.frame(str_match(input, "([A-Z])(\\d+)")[,-1])
     input %>% 
         mutate(V2 = as.numeric(V2), 
-               V2 = case_when(V1 == "W" ~-V2,
-                              V1 == "S" ~-V2,
-                              V1 == "L" ~360-V2, 
+               V2 = case_when(V1 == "R" ~360-V2, 
                               TRUE ~ V2))
     
 }
 
 input <- parse_input(input)
 
-# Part 1, only ship moves
-part1 <- function(input, east = 0, north = 0, direction = 1){
-    directions <- setNames(c(0,   1,   2,   3), 
-                           c('N', 'E', 'S', 'W'))
-    curr_dir <- names(directions)[direction+1]
+# Using complex plane system! New trick discovered
+
+movement <- list(E = 1 +0i, 
+                 W = -1 + 0i,
+                 N = 0 + 1i,
+                 S = 0 - 1i)
+
+move_ship <- function(input, waypoint = (1 + 0i), ship = (0 + 0i), part1 = TRUE){
     for(i in 1:nrow(input)){
-        curr_step <- input[i, 1]
+        cur_step <- input[i, 1]
         count <- input[i, 2]
-        if(curr_step == "F"){
-            curr_step <- curr_dir
-            if(curr_step %in% c("W", "S"))
-                count <- -count
-        }
-        if(curr_step %in% c("W", "E"))
-            east <- east + count
-        if(curr_step %in% c("N", "S"))
-            north <- north + count
-        if(curr_step %in%  c("R", "L")){
-            direction <- (direction + count / 90) %% 4
-            curr_dir <- names(directions)[direction+1]
+        if(cur_step %in% c("R", "L")){
+            count <- count / 90
+            waypoint <- waypoint * (0 + 1i)^count
+        } else if(cur_step == "F"){
+            ship <- ship + waypoint * count
+        } else if(part1){
+            ship <- ship + movement[[cur_step]] * count
+        } else {
+            waypoint <- waypoint + movement[[cur_step]] * count
         }
     }
-    sum(abs(east), abs(north))
+    abs(Re(ship)) + abs(Im(ship))
 }
-part1(input)
 
-# Part 2 ship and waypoint move
-part2 <- function(input, wp_n = 1, wp_e = 10, sh_n = 0, sh_e = 0){
-    for(i in 1:nrow(input)){
-        curr_step <- input[i,1]
-        count <- input[i, 2]
-        
-        if(curr_step == "F"){
-            sh_n <- sh_n + count * wp_n
-            sh_e <- sh_e + count * wp_e
-        }
-        if(curr_step %in% c("N", "S")){
-            wp_n <- wp_n + count
-        }
-        if(curr_step %in% c("E", "W")){
-            wp_e <- wp_e + count
-        }
-        if(curr_step %in% c("R", "L")){
-            if(count == 90){
-                tmp <- wp_n
-                wp_n <- -wp_e
-                wp_e <- tmp
-            }
-            if(count == 180){
-                wp_n <- -wp_n
-                wp_e <- -wp_e
-            }
-            if(count == 270){
-                tmp <- wp_n
-                wp_n <- wp_e
-                wp_e <- -tmp
-                
-            }
-        }
-    }
-    sum(abs(sh_e), abs(sh_n))
-}
-part2(input)
+# part 1
+move_ship(input)
 
+# part 2
+move_ship(input, waypoint = (10 + 1i), ship = (0+0i), part1 = FALSE)
 
 
 
