62a4ad7f0f83219ee2230da5fb38d91f9a4964b0
AdroMine <adro.mine@outlook.com>
2020-12-14 02:02:57 +0530

    day 13 minor cleaning

--- Day13/solution.R
+++ Day13/solution.R
@@ -10,56 +10,42 @@ bus_t <- as.numeric(buses[buses!="x"])
 wait_time <- ((target %/% bus_t + 1) * bus_t) - target   # %/% is integer division in R
 
 idx <- which.min(wait_time)
-wait_time[idx] * bus_t[idx]
+wait_time[idx] * bus_t[idx] # ans1
 
 
 # for higher precision, wasted too much time because of 
 # precision error
 library(Rmpfr)
+# modInverse using euler prime number 
+modInverse <- function(a, m){
+  return(numbers::modpower(a%%m, m-2, m))
+}
 
 # Part 2
 part2 <- function(buses){
     dept <- which(buses!="x") - 1
     buses <- as.double(buses[buses!="x"])
     
-    .N <- function(.) mpfr(., precBits = 200)    
-    N <- .N(prod(buses))
+    .N <- function(.) mpfr(., precBits = 100) # fails below precBits = 58    
+    N <- prod(buses)
     Y <- N/buses
-    rem <- -dept # can also use -dept %% buses
-    inv <- rep(NA, length(buses))
+    rem <- -dept %% buses
+    inv <- sapply(1:length(buses), function(i) modInverse(Y[i], buses[i]))    
     
-    # inverse modulo
-    for(i in 1:length(buses)){
-        # n *k %% m equivalent to [(n %% m)*(k%%m)] %% m 
-        m <- Y[i] %% buses[i]
-        k <- 1:(buses[i]-1)
-        remainder <- (m*k) %% buses[i]
-        inv[i] <- which(remainder == 1)
-    }
+    Y <- .N(Y) # increase precision
     sum(Y * rem * inv) %% N
 }
 part2(buses)
 
 
-# modInverse using euler extended
-# modInverse <- function(a, m){
-#   rem <- m
-#   y <- 0
-#   x <- 1
-#   while(a > 1){
-#       q <- a %/% rem
-#       tmp <- rem
-#       rem <- a %% rem
-#       a <- tmp
-#       tmp <- y
-#       y <- x - q*y
-#       x <- tmp
-#   }
-#   if(x < 0)
-#       x <- x + m
-#   return(x)
+# inv <- rep(NA, length(buses))
+# # inverse modulo
+# for(i in 1:length(buses)){
+#     # n *k %% m equivalent to [(n %% m)*(k%%m)] %% m 
+#     m <- Y[i] %% buses[i]
+#     k <- 1:(buses[i]-1)
+#     remainder <- (m*k) %% buses[i]
+#     inv[i] <- which(remainder == 1)
 # }
-# 
-# inv <- sapply(1:length(buses), function(i) modInverse(Y[i], buses[i]))    
 
 
