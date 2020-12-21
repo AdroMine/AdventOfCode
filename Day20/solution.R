bfe74d2c13b6ae934009bb3c0b771fcc587281cb
AdroMine <adro.mine@outlook.com>
2020-12-22 01:48:42 +0530

    refactored day 20

--- Day20/solution.R
+++ Day20/solution.R
@@ -2,123 +2,107 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 library(stringr)
 library(purrr)
-library(dplyr) # for between
+library(dplyr)
 input <- readLines("input.txt")
 
-st <- grep("^Tile", input)
+start <- grep("^Tile", input)
 
-tiles <- map(st, function(x) str_split_fixed(input[(x + 1): (x + 10)], "", Inf))
-names(tiles) <- str_extract(input[st], "\\d+")
+tiles <- map(start, function(x) str_split_fixed(input[(x + 1): (x + 10)], "", Inf))
+names(tiles) <- str_extract(input[start], "\\d+")
 
 nghbrs <- matrix(rep(c( NA,    NA,     NA,      NA,     NA,     NA,     NA,     NA), length(tiles)), nrow = length(tiles))
 colnames(nghbrs) <- c("left", "lrev", "up", "urev", "right", "rrev", "down", "drev")
 
 # CONSTANTS
 E <- 10  # edge length
-R <- sqrt(length(tiles))
-C <- R
+C <- R <- sqrt(length(tiles))
 
 get_sides <- function(tile){
-    l <- tile[,1]
-    r <- tile[,10]
-    u <- tile[1,]
-    d <- tile[10,]
-    tmp <- unlist(lapply(list(l, u, r, d), function(x)
-        c(paste(x, collapse = ""), paste(rev(x), collapse = ""))))
-    names(tmp) <- c("left", "lrev", "up", "urev","right", "rrev","down", "drev")
-    tmp
+    l <- tile[  ,1] ; r <- tile[  ,E]
+    u <- tile[1,  ] ; d <- tile[E,  ]
+    unlist(lapply(list(l, u, r, d), 
+                  function(x) c(paste(x, collapse = ""), 
+                                paste(rev(x), collapse = ""))))
 }
 
 # Do two tiles match
-matcher <- function(i, j,n = 10){
+matcher <- function(i, j){
     t1 <- tiles[[i]]
     t2 <- tiles[[j]]
     
     t1s <- get_sides(t1)
     t2s <- get_sides(t2)[c(1, 3, 5, 7)]
     
-    mtch <- outer(t1s, t2s, function(x,y) x == y)
-    if(any(mtch)){
-        # matches!
-        idx <- which(mtch, arr.ind = TRUE)
-        return(idx)
-    } else
-        return(NA)
+    any(outer(t1s, t2s, function(x,y) x == y))  # any matches between all sides
 }
 
-# find which edge matches which edge
-for(i in 1:(length(tiles)-1)){
-    for(j in (i + 1):length(tiles)){
-        # print(paste0("i:", i, ";  j=",j))
-        idx <- matcher(i, j)
-        if(any(!is.na(idx))){
-            t1 <- idx[,1]
-            t2 <- idx[,2] * 2 - 1
-            nghbrs[i, t1] <- j
-            nghbrs[j, t2] <- i
-        } 
-    }
-}
+# Determine which tiles matches which tile
+mtch <- expand.grid(i = 1:length(tiles), j = 1:length(tiles)) %>% filter(i!=j)
+mtch$match <- apply(mtch, 1, function(x) matcher(x[1], x[2]))
+
+# `nghbrs` becomes a list desribing which tile has which members 
+# tile[[x]] = adjacent tile ids
+nghbrs <- mtch %>% 
+    mutate(j = ifelse(match, j, -1)) %>% filter(j != -1) %>% 
+    group_by(i) %>% summarise(match = list(j)) %>%
+    deframe()
+
 # Part 1 Ans
-corners <- which(rowSums(!is.na(nghbrs)) == 2)
+corners <- which(lapply(nghbrs, length) == 2)
 print(prod(as.numeric(names(tiles)[corners])), 22)
 
-# let top left corner be any corner, we can rotate board so that it is the top left
-current <- corners[1]
+# Join Image
 
+# start with empty board and let top left corner be any corner, we can rotate board so that it is the top left
 board <- new.env()
 board$g <- matrix(NA, sqrt(length(tiles)), sqrt(length(tiles)))
-board$g[1, 1] <- current
+board$g[1, 1] <- current <- corners[1]
 
-nxt <- as.integer(na.omit(nghbrs[current,]))
+nxt <- nghbrs[current][[1]]
+board$g[1,2] <- nxt[1]         # place tiles adjacent to corner to random adjacent cells
+board$g[2,1] <- nxt[2]         # (it's all symmetric if you flip it around)
 
-board$g[1,2] <- nxt[1]
-board$g[2,1] <- nxt[2]
-
-
+# list of possible adjacent cell coordinates for any tile
 adjacent <- list(c(0,1), c(1,0), c(-1, 0), c(0,-1))
 bR <- nrow(board$g)
 bC <- ncol(board$g)
-keys_left <- setdiff(1:(R*C), c(current, nxt))
+pieces_left <- setdiff(1:(R*C), c(current, nxt)) # tiles not placed
 
-while(anyNA(board$g)){
-    for(i in 1:bR){
+while(anyNA(board$g)){  # while board is empty go through every cell
+    for(i in 1:bR){     # at any particular cell
         for(j in 1:bC){
-            # print(sprintf("(i,j) = (%d, %d)", i,j))
             if(is.na(board$g[i,j])){
-                possible <- keys_left
-                for(adj in adjacent){
+                possible <- pieces_left
+                for(adj in adjacent){      # look at ajdacent cells if not empty
                     ii <- i + adj[1]
                     jj <- j + adj[2]
                     if((ii %in% 1:bR) && (jj %in% 1:bC) && !is.na(board$g[[ii,jj]] )){
-                        possible <- intersect(possible, nghbrs[board$g[ii,jj],])
+                        possible <- intersect(possible, 
+                                              nghbrs[board$g[ii,jj]][[1]])  # and reduce possibilities
                     }
                 }
-                if(length(possible)==1){
+                if(length(possible)==1){    # correct tile will satisfy all adjacent cells and will be unique (at least here)
                     board$g[i,j] <- possible
-                    keys_left <- setdiff(keys_left, possible)
+                    pieces_left <- setdiff(pieces_left, possible)
                 }
-                
             }
         }
     }
 }
 
+# function to get all possible flip and rotations of a matrix
 rot_flip <- function(mat){
-    p1 <- mat                            # Orig
-    p2 <- apply(mat, 2, rev)             # Vertical flip
-    # p2 <- t(apply(mat, 1, rev)) # Horizontal flip
-    # rotations of above two
-    p3 <- t(apply(p1, 2, rev))
-    p4 <- t(apply(p3, 2, rev))
-    p5 <- t(apply(p4, 2, rev))
-    
-    p6 <- t(apply(p2, 2, rev))
-    p7 <- t(apply(p6, 2, rev))
-    p8 <- t(apply(p7, 2, rev))
-    list(p1, p2, p3, p4, p5, p6, p7, p8)
+    ret <- vector("list", 8)
+    p1 <- ret[[1]] <- mat                            # Orig
+    p2 <- ret[[5]] <- apply(mat, 2, rev)             # Vertical flip
+    for(i in 2:4){
+        p1 <- ret[[i]]   <- t(apply(p1, 2, rev))     # rotation clockwise 90
+        p2 <- ret[[4+i]] <- t(apply(p2, 2, rev))     # rotation clockwise 90
+    }
+    ret
 }
 
+# Do two tiles match in a particular orientation?
 tiles_matcher <- function(tile1, tile2, adj1, adj2){
     if( adj2 == 1 ){ # tile2 on right of tile1
         return(all(tile1[,E] == tile2[,1]))
@@ -135,59 +119,66 @@ tiles_matcher <- function(tile1, tile2, adj1, adj2){
 }
 
 # Now Orient
-for(i in 1:R){
+for(i in 1:R){                     # For every cell
     for(j in 1:C){
         t1 <- board$g[i,j]
-        p1 <- rot_flip(tiles[[t1]])
+        p1 <- rot_flip(tiles[[t1]]) # get all possible tile orientations
         selected <- 1:length(p1)
-        for(adj in adjacent){
+        for(adj in adjacent){       # then for each adjacent cell
             ii <- i + adj[1]
             jj <- j + adj[2]
             if( (ii %in%  1:R) && (jj %in% 1:C) ){
                 t2 <- board$g[ii,jj]
-                p2 <- rot_flip(tiles[[t2]])
+                p2 <- rot_flip(tiles[[t2]])        # get all possible orientations
                 k <- expand.grid(a = 1:8, b = 1:8)
-                idx <- k %>% 
-                    rowwise() %>% 
-                    mutate(match = tiles_matcher(p1[[a]], p2[[b]], adj[1], adj[2])) %>% 
-                    filter(match == TRUE) %>% 
-                    pull(a) %>% unique()
-                selected <- intersect(selected, idx)
+                m <- apply(k, 1, function(x) tiles_matcher(p1[[x[1]]], p2[[x[2]]], adj[1], adj[2])) # which orientations match?
+                idx <- unique(k$a[which(m)])
+                selected <- intersect(selected, idx)  # for each adjacent cell keep on reducing possible orientations of current cell
             }
         }
-        assertthat::assert_that(length(selected) == 1)
+        assertthat::assert_that(length(selected) == 1) # one unique orientation available, select it
         tiles[[t1]] <- p1[[selected]]
     }
 }
 
-image <- as.matrix(bind_rows(lapply(1:bR, function(i){
-    bind_cols(lapply(board$g[i,], function(x) tiles[[x]][2:(E-1), 2:(E-1)]))  
-})))
+###  CREATE IMAGE ###
+# cbind all board rows and then rbind together the results
+image <- as.matrix(
+    bind_rows(
+        lapply(1:bR,           # for all rows in board
+               function(i){
+                   bind_cols(  # horizontally stack (cbind) all tiles
+                       lapply( board$g[i,], 
+                               function(x) tiles[[x]][ 2:(E-1), 2:(E-1) ]) ) 
+               }
+        )
+    )
+)
 dimnames(image) <- NULL
 MR <- nrow(image)
 MC <- ncol(image)
 
+### MONSTER
+# create monster as a matrix which we will `slide` over our image to check for match
 monster <- unlist(strsplit(c("                  # ",
                              "#    ##    ##    ###",
                              " #  #  #  #  #  #   "), ""))
-monster <- matrix(monster, nrow = 3, byrow = T)
-monster[monster == " "] <- NA
+monster <- matrix(monster, nrow = 3, byrow = T)   
+monster[monster == " "] <- NA           # since spaces don't count turn them to NA for easier matches later
 
-
-
-images <- rot_flip(image)            # Get all possible variations of image
+images <- rot_flip(image)            # Get all possible orientations of image
 s <- ncol(monster)
 for(img in images){
     monster_found <- FALSE
-    
+    monst <- 0
     # sliding window approach
     for(i in 1:(MR-2)){
         for(j in 1:(MC-s+1)){
             ind1 <- i: (i + 2)
             ind2 <- j:(j + s-1)
-            slide <- img[ind1, ind2] == monster
-            if(all(slide, na.rm = TRUE)){
-                print("found it!")
+            slide <- img[ind1, ind2] == monster   # compare
+            if(all(slide, na.rm = TRUE)){         # if match
+                monst <- monst + 1
                 img[ind1,ind2][slide] <- "O"
                 monster_found <- TRUE
             }
@@ -197,3 +188,4 @@ for(img in images){
         break
 }
 sum(img == "#")
+sprintf("Monsters found: %d", monst)
