4713bd9f0afe0bb30da22341bccb27ffa9aa88f4
AdroMine <adro.mine@outlook.com>
2020-12-27 14:16:04 +0530

    some cleaning up

--- Day 05/solution.R
+++ Day 05/solution.R
@@ -2,8 +2,10 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 seat_ids <- readLines("input.txt")
 
 # Seat IDs are binary numbers, convert F and L -> 0 B and R -> 1
-seat_ids <- gsub("F|L", "0", seat_ids)
-seat_ids <- gsub("B|R", "1", seat_ids)
+seat_ids <- chartr("FLBR", "0011", seat_ids)
+
+# seat_ids <- gsub("F|L", "0", seat_ids)
+# seat_ids <- gsub("B|R", "1", seat_ids)
 
 # convert binary to integer
 ids <- strtoi(seat_ids, 2)
--- Day 07/solution.R
+++ Day 07/solution.R
@@ -31,39 +31,3 @@ ans2 <- function(bag_col){
 }
     
 ans2('shiny gold')    
-    
-
-# Tidyverse approach
-# Get inside part
-# Create data frame
-df <- bind_cols(bag = bags, contain)
-names(df)[2:5] <- paste0("rules",1:4)
-
-# convert to long form
-df2 <- df %>% 
-    pivot_longer(-bag) %>% 
-    mutate(value = na_if(value, "")) %>% 
-    na.omit() %>% 
-    separate(value, into = c("number", "colour"), 
-             convert = TRUE, extra = "merge") %>% 
-    mutate(number = as.integer(str_replace(number, "no", "0")), 
-           colour = gsub("^([a-z ]+) ba.*$", "\\1", colour)) %>% 
-    select(-name) 
-
-bag_list <- df2 %>% select(bag, colour, number) %>% nest(data = -bag) %>% deframe()
-bag_list <- lapply(bag_list, deframe)
-
-# Answer to part 1
-k <- filter(df2, colour == "shiny gold") %>% pull(bag)
-total <- k
-while(length(k) > 0){
-    # message("\n")
-    # message(paste(k, collapse = ","))
-    k <- unique(filter(df2, colour %in% k) %>% pull(bag))
-    total = c(total,k)
-}
-
-length(unique(total))
-
-filter(df2, bag == "shiny gold") %>% select(colour, number) %>% deframe()
-
