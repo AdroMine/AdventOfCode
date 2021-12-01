a717ada6a9254986c5460f735f02cee0dcdcf138
AdroMine <adro.mine@outlook.com>
2021-12-01 21:01:46 +0530

    optimised day 1 R

--- 2021/Day01/solution.R
+++ 2021/Day01/solution.R
@@ -21,3 +21,7 @@ sliding_sums <- data.table::frollsum(input, n = 3)
 increases <- sliding_sums - lag(sliding_sums)
 
 sum(increases > 0, na.rm = TRUE)
+
+
+# Part 2 optimised
+sum(input > lag(input, 3), na.rm = TRUE)
