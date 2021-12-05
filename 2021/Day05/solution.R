57469b6ea299a7d4ab90d4f0355101eec7527c16
AdroMine <adro.mine@outlook.com>
2021-12-05 15:10:19 +0530

    day5: more concise solution

--- 2021/Day05/solution.R
+++ 2021/Day05/solution.R
@@ -15,7 +15,7 @@ overlapping_lines <- function(m, part2 = FALSE){
     
     for(pair in coords_pairs){
         
-        start <- as.numeric(strsplit(pair[[1]], ",")[[1]]) + 1 # since index starts at 
+        start <- as.numeric(strsplit(pair[[1]], ",")[[1]]) + 1 # since index starts at 1
         end   <- as.numeric(strsplit(pair[[2]], ",")[[1]]) + 1
         
         # vertical line (same x)
@@ -42,3 +42,30 @@ overlapping_lines <- function(m, part2 = FALSE){
 # part1 
 overlapping_lines(m, FALSE)
 overlapping_lines(m, TRUE)
+
+
+
+
+# Smaller Code, don't need all if else
+
+overlapping_lines2 <- function(m, part2 = FALSE){
+    
+    for(pair in coords_pairs){
+        
+        start <- as.numeric(strsplit(pair[[1]], ",")[[1]]) + 1 # since index starts at 1
+        end   <- as.numeric(strsplit(pair[[2]], ",")[[1]]) + 1
+        
+        if(part2){ # draw for all pairs
+            m[cbind(seq(start[2], end[2]), seq(start[1], end[1]))] <- m[cbind(seq(start[2], end[2]), seq(start[1], end[1]))] + 1
+        }
+        else {     # for part 1, don't consider diagonal lines
+            if(start[1] == end[1] || start[2] == end[2])
+                m[cbind(seq(start[2], end[2]), seq(start[1], end[1]))] <- m[cbind(seq(start[2], end[2]), seq(start[1], end[1]))] + 1
+        }
+    }  
+    sum(m >= 2)
+}
+
+
+overlapping_lines2(m, FALSE)
+overlapping_lines2(m, TRUE)
