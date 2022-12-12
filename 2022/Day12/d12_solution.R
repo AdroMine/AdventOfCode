da6f5bcd8fd342de7527f9b34243621faf7e3564
AdroMine <adro.mine@outlook.com>
2022-12-12 22:15:33 +0530

    update day 12

--- 2022/Day12/d12_solution.R
+++ 2022/Day12/d12_solution.R
@@ -5,26 +5,27 @@ file <- 'input.txt'
 width <- nchar(readLines(file, n = 1))
 input <- as.matrix(read.fwf(file, widths = rep(1, width)))
 
-
-
-dijkstra <- function(graph, start = which(graph == 'S', arr.ind = TRUE)){
+dijkstra <- function(graph, start = 'S', goal = 'E', part2 = FALSE){
     
     N <- nrow(graph)
     C <- ncol(graph)
-    goal <- which(graph == 'E', arr.ind = TRUE)
-    # start <- which(graph == 'S', arr.ind = TRUE)
-    
-    graph[start] <- 'a'
-    graph[goal] <- 'z'
+    goal <- which(graph == goal, arr.ind = TRUE)
+    start <- which(graph == start, arr.ind = TRUE)
+   
+    if(!part2){
+        graph[start] <- 'a'
+        graph[goal] <- 'z'
+    } else {
+        graph[start] <- 'z'
+    }
     
     openset <- collections::priority_queue()
     openset$push(start, priority = 0)
     
-    visited <- matrix(FALSE, N, C)
-    
-    # gscore
+    # score
     mindist <- matrix(Inf, N, C)
     mindist[start[1], start[2]] <- 0
+    
     neighbours <- function(xy){
         x <- xy[1]
         y <- xy[2]
@@ -42,13 +43,11 @@ dijkstra <- function(graph, start = which(graph == 'S', arr.ind = TRUE)){
                 FALSE
             } else {
                 pt <- utf8ToInt(graph[nx, ny])
-                (pt - cur) <= 1
+                if(!part2) (pt - cur) <= 1 else (pt - cur) >= -1
             }
         })
         nbrs[possible]
-        
     }   
-       
     
     while(openset$size() > 0){
         
@@ -57,10 +56,9 @@ dijkstra <- function(graph, start = which(graph == 'S', arr.ind = TRUE)){
         cy <- curr[2]
         d <- mindist[cx,cy]
         
-        if(all(curr == goal))
-            break
-        
-        visited[cx, cy] <- TRUE
+        if(part2){
+            if(graph[cx, cy] == 'a') return(d)
+        } else if(all(curr == goal)) break
         
         adjacent <- neighbours(curr)
         
@@ -84,19 +82,74 @@ dijkstra <- function(graph, start = which(graph == 'S', arr.ind = TRUE)){
 dijkstra(input)
 
 # Part 2 
+# find shortest path from E to any 'a'
+dijkstra(input, 'E', 'a', part2 = TRUE)
 
-# points that start with 'a'
-a_starts <- which(input == 'a', arr.ind = TRUE)
-N <- nrow(a_starts)
-res <- vector('double', N)
 
-# for each start point, perform dijkstra, (takes around 1 min)
-# quite fast after the first 100 starting points
-for(i in seq_along(res)){
-    start <- a_starts[i,]
-    print(sprintf("%d / %d", i, N))
-    res[i] <- dijkstra(input, start)
+# BFS
+
+next_points <- function(curr){
+    next_pts <- list(
+        c(0, 1), c(0, -1), c(-1, 0), c(1, 0)
+    )
+    lapply(next_pts, \(x) x + curr)
 }
-min(res)
 
 
+bfs <- function(graph, part2){
+    
+    N <- nrow(graph)
+    C <- ncol(graph)
+    goal <- which(graph == 'E', arr.ind = TRUE)
+    start <- which(graph == 'S', arr.ind = TRUE)
+    graph[start] <- 'a'
+    graph[goal] <- 'z'
+    
+    visited <- matrix(FALSE, N, C)
+    
+    Q <- collections::deque()
+    if(!part2){
+        Q$push(c(start, 0))
+        
+    } else {
+        starting_points <- which(graph == 'a', arr.ind = TRUE)
+        for(i in 1:nrow(starting_points)){
+            Q$push(c(starting_points[i,1], starting_points[i, 2], 0))
+        }
+    }
+    
+    while(Q$size()){
+        
+        curr <- Q$popleft()
+        rx <- curr[1]
+        cy <- curr[2]
+        d  <- curr[3]
+        
+        if(visited[rx, cy]) next 
+        visited[rx, cy] <- TRUE
+        
+        if(all(curr[-3] == goal)) {
+            return(d)
+        }
+        
+        cur_pt <- utf8ToInt(graph[rx, cy])
+        
+        for(adj in next_points(c(rx, cy))){
+            nx <- adj[1]
+            ny <- adj[2]
+            if(nx > N || ny > C || nx < 1 || ny < 1){
+                next
+            } 
+            pt <- utf8ToInt(graph[nx, ny])
+            if((pt - cur_pt) <= 1){
+                Q$push(c(nx, ny, d + 1))
+            }
+        }
+    }
+}
+
+# Part 1
+bfs(input, FALSE)
+
+# Part 2
+bfs(input, TRUE)
--- 2022/Day12/readme.md
+++ 2022/Day12/readme.md
@@ -8,6 +8,10 @@
 
 Apply dijkstra's algorithm for part 1 and 2. 
 
-For part2, currently apply dijkstra for all start points. Need to find better method for this.
+~~For part2, currently apply dijkstra for all start points. Need to find better method for this~~
+
+For part2, start at 'E' and search for 'a'. Reverse path finding. 
+
+We could also just use BFS since weights are constant. For part1, we start off with a Deque containing c('S', 0) while for part2 the Deque is initialised with all points that are 'a'
 
 
