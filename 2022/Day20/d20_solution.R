beee266fdb5ceaf12991cf088a52441a4b499a29
AdroMine <adro.mine@outlook.com>
2022-12-20 23:18:09 +0530

    day 20

---
+++ 2022/Day20/d20_solution.R
@@ -0,0 +1,66 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(magrittr)
+
+input <- read.table('input.txt')[[1]]
+
+sample <- c(1, 2, -3, 3, -2, 0, 4)
+
+# positions <- 1:N
+
+get_n <- function(v, n, st) v[(st + n-1) %% length(v) + 1] 
+asc <- as.character
+
+res <- input 
+names(res) <- asc(1:length(res))
+N <- length(res)
+
+for(i in seq_along(res)){
+
+    cur_pos <- which(asc(i) == names(res))
+    move <- res[asc(i)]
+    if(move == 0) next
+    
+    new_pos <- (cur_pos - 1L + move) %% (N-1) + 1
+    if(new_pos > cur_pos){
+        left <- res[setdiff(seq_len(new_pos), cur_pos)] # from start to new_pos except current
+        right <- res[new_pos + seq_len(N - new_pos)]    # from new_pos to end
+        
+    } else if(new_pos < cur_pos) {
+        left <- res[seq_len(new_pos - 1L)] # from start to one less than new pos
+        right <- res[setdiff(seq.int(new_pos, N), cur_pos)] # from new pos to end
+    }
+    res <- c(left, move, right)
+}
+
+zero_idx <- which(res == 0)
+
+sum(get_n(res, c(1000, 2000, 3000), zero_idx))
+
+
+dec_key <- 811589153
+res <- input * (dec_key)
+N <- length(res)
+names(res) <- asc(1:length(res))
+for(loop in 1:10){
+    # print(loop)
+    # print(length(res))
+    # stopifnot(!anyNA(res))
+    for(i in seq_along(res)){
+        cur_pos <- which(asc(i) == names(res))
+        move <- res[asc(i)]
+        
+        new_pos <- (cur_pos - 1L + move) %% (N-1) + 1
+        if(new_pos == cur_pos || move == 0) next
+        if(new_pos > cur_pos){
+            left <- res[setdiff(seq_len(new_pos), cur_pos)] # from start to new_pos except current
+            right <- res[new_pos + seq_len(N - new_pos)]    # from new_pos to end
+            
+        } else if(new_pos < cur_pos) {
+            left <- res[seq_len(new_pos - 1L)] # from start to one less than new pos
+            right <- res[setdiff(seq.int(new_pos, N), cur_pos)] # from new pos to end
+        }
+        res <- c(left, move, right)
+    }   
+}
+
+get_n(res, c(1000, 2000, 3000), which(res == 0))
--- 2022/Day20/readme.md
+++ 2022/Day20/readme.md
@@ -1,7 +1,18 @@
-# Day 20: [](https://adventofcode.com/2022/day/20)
+# Day 20: [Grove Positioning System](https://adventofcode.com/2022/day/20)
 
 ### Question Summary
-- **Part 1** - 
-- **Part 2** - 
+- **Part 1** - Shift number right/left based on its value. Do this for each number in the order they appear. 
+- **Part 2** - Multiply by big number and repeat above 10 times. 
 
 ### Solution summary 
+
+We need to keep track of positions. We can do this easily by using names of the vector and storing the actual positions as character names for the vector. We will thus have:
+
+> `1  2  3  4  5` -> names
+ `n1 n2 n3 n4 n5` -> values
+
+Then even when the values have shifted, the names shift with them, so we can use them to identify which needs to be rotated next. 
+
+For rotation, instead of performing modulus with N, use N-1, since it is a circular array and start and end are adjacent to each other. 
+
+
