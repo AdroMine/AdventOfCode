45ecfd0fce54da1875aebe73d5fd33347ee906b6
AdroMine <adro.mine@outlook.com>
2023-12-06 14:36:31 +0530

    2023 day6: add algebraic method

--- 2023/Day06/readme.md
+++ 2023/Day06/readme.md
@@ -9,3 +9,44 @@
 Uses vectorised method. 
 
 Part 2 also uses the same method and doesn't run into any problem. 
+
+
+For non brute force method, we can generate the following equation:
+
+
+We have equation for distance travelled:  $x * (T - x)$
+
+where $x$ is time we pause in the beginning (and therefore the spped)
+
+while $T$ is the total time available for the given race. 
+
+If we have $D$ as the previous distance that needs to be beaten, then we want:
+
+
+ $xT - x^2 > D$
+
+or 
+
+$-x^2 + xT - D > 0$
+
+We can thus solve for $-x^2 + xT - D = 0$ which is a quadratic equation and gives a parabola that opens downward. The two solutions for this equationi will be the two `speeds` for which we reach the previous recorded distance. Any valid speed between these points will take us further than the previous record. 
+
+The solution for quadratic equation is:
+
+$$
+x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}
+$$
+
+Which for our case becomes:
+
+$$ 
+x = \frac{-T \mp \sqrt{(-T)^2 - 4 \times (-1) \times (-D)}}{2 \times -1}
+$$
+
+or
+
+$$
+x = \frac{T \mp \sqrt{T^2 - 4D}}{2}
+$$
+
+Once we get two values of `x` here, we find the integers between these, which is the number of ways that can be used to win. 
--- 2023/Day06/solution.R
+++ 2023/Day06/solution.R
@@ -6,9 +6,9 @@ input <- readLines(file_name) |> stringr::str_extract_all('\\d+')
 times <- as.numeric(input[[1]])
 distances <- as.numeric(input[[2]])
 
-ways_to_win <- vector('numeric', length(time))
+ways_to_win <- vector('numeric', length(times))
 
-for(i in seq_along(time)){
+for(i in seq_along(times)){
   
   tm <- times[i]
   dist <- distances[i]
@@ -21,9 +21,10 @@ for(i in seq_along(time)){
   
   ways_to_win[i] <- wins
   
-  
-  
 }
+
+# Part 1
+prod(ways_to_win)
 
 # Part 2
 time <- as.numeric(paste0(input[[1]], collapse = ''))
@@ -33,3 +34,38 @@ hold_down_for <- seq_len(time - 1)
 dist_travelled <- hold_down_for * (time - hold_down_for)
 
 wins <- sum(dist_travelled > dist)
+wins
+
+
+# Non Brute force
+# We have equation for distance travelled
+# x * (T - x)
+# We want -> xT - x^2 > D
+# -x2 + xT - D > 0
+# We can solve for -x2 + xT -D = 0 (quadratic equation)
+# we will get two points where the parabola meets the horizontal axis. Any points between 
+# them will be valid points that will lead to winning situation
+# quadratic formula x = (-b + c(-1, 1) * root(b^2 - 4ac)) / 2(a)
+# x = (-T + c(-1, 1) * (T^2 - 4 * (-1)*(-D))^(1/2)) / (2 * (-1))
+# x = (T - c(-1, 1) * (T^2 - 4D)^0.5) / (2)
+
+quad_sol <- function(time, distance){
+  
+  sort(floor((time - c(-1, 1) * (time^2 - 4*distance)^0.5) / 2))
+  
+}
+
+num_win_methods <- function(time, distance){
+  diff(quad_sol(time, distance))
+}
+
+# Part 1
+mapply(num_win_methods, times, distances) |> prod()
+
+
+# Part 2
+
+time <- as.numeric(paste0(input[[1]], collapse = ''))
+dist <- as.numeric(paste0(input[[2]], collapse = ''))
+
+num_win_methods(time, dist)
