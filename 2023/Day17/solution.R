bae47574acc35694636616469a0e4c42f30d7e62
AdroMine <adro.mine@outlook.com>
2023-12-17 18:48:51 +0530

    2023 day 17 optimised, now runs in 5-6 seconds

--- 2023/Day17/solution.R
+++ 2023/Day17/solution.R
@@ -7,39 +7,32 @@ W <- nchar(readLines(file_name, n = 1))
 input <- as.matrix(read.fwf(file_name, widths = rep(1, W), colClasses = 'integer'))
 mode(input) <- 'integer'
 
+# generate the four possible new directions to move in along with the direction 
+# the new movement would be in and consecutive steps
 next_points <- function(curr, direction, consecutive_steps, min_steps, max_steps){
-  next_pts <- list(
-    'right' = c(0, 1, 1),  # right coordinates movement + consecutive step
-    'left'  = c(0, -1, 1), # left
-    'up'    = c(-1, 0, 1), # up
-    'down'  = c(1, 0, 1) # down
-  )
-  # can't move back
-  nxt_pts <- switch(
-    direction, 
-    'right' = next_pts[-2], 
-    'left'  = next_pts[-1], 
-    'up'    = next_pts[-4], 
-    'down'  = next_pts[-3]
-  )
+  # direction top right down left
+  # directions + new steps + new_direction
+  next_pts <- list(c(-1, 0, 1, 1), c(0, 1, 1, 2), c(1, 0, 1, 3), c(0, -1, 1, 4))
+  next_pts[[direction]][3] <- consecutive_steps + 1
+  
+  # keep direction from 1 to 4 (north, east, south, west)
+  
+  # remove reverse direction
+  to_remove <- (direction + 2 - 1) %% 4 + 1 
+  
   # if less than 4 steps, only move in that direction
   if(consecutive_steps < min_steps){
-    nxt_pts <- nxt_pts[which(names(nxt_pts) == direction)]
-  }
+    return(next_pts[direction])
+  } 
+  
   if(consecutive_steps == max_steps){
-    nxt_pts <- nxt_pts[-which(names(nxt_pts) == direction)] # can't go in same direction anymore
+    to_remove <- c(direction, to_remove)
   }
+  next_pts <- next_pts[-to_remove]
   
-  # increment step taken in current direction, all else set to 1
-  if(direction %in% names(nxt_pts)){
-    nxt_pts[[which(names(nxt_pts) == direction)]][3] <- consecutive_steps + 1
-  }
+  next_pts
   
-  lapply(nxt_pts, \(x) c(x[-3] + curr, x[3]))
 }
-
-directions <- c('up', 'down', 'left', 'right')
-dir_name_to_idx <- c('up' = 1, 'down' = 2, 'left' = 3, 'right' = 4)
 
 
 dijkstra <- function(graph, part2){
@@ -49,10 +42,10 @@ dijkstra <- function(graph, part2){
   goal <- c(N,C)
   start <- c(1,1)
   
-  # directions up down left right - 1 2 3 4
+  # directions north, east, south, west - 1, 2, 3, 4
   # coordinates, direction, consecutive steps
   openset <- collections::priority_queue()
-  initial_d <- c(start, 4, 0)
+  initial_d <- c(start, 2, 0) # start from top left, going right, no consecutive steps now
   openset$push(initial_d, priority = 0) 
   
   # score
@@ -62,32 +55,24 @@ dijkstra <- function(graph, part2){
   while(openset$size() > 0){
     
     curr <- openset$pop()
-    cx  <- curr[1]
-    cy  <- curr[2]
-    dir <- directions[curr[3]] # direction
-    cs  <- curr[4] # consecutive steps
-    
     d <- mindist$get(curr, Inf)
     
     if(!part2){
-      adjacent <- next_points(curr[1:2], dir, cs, 1, 3)
+      adjacent <- next_points(curr[1:2], curr[3], curr[4], 1, 3)
     } else {
-      adjacent <- next_points(curr[1:2], dir, cs, 4, 10)
+      adjacent <- next_points(curr[1:2], curr[3], curr[4], 4, 10)
     }
     
     for(i in seq_along(adjacent)) {
       nbr  <- adjacent[[i]]
-      nx   <- nbr[1]
-      ny   <- nbr[2]
-      ndir <- unname(dir_name_to_idx[names(adjacent)[i]])
-      cs   <- nbr[3]
-      
+      nx   <- nbr[1] + curr[1]
+      ny   <- nbr[2] + curr[2]
       if(nx > N || ny > C || nx < 1 || ny < 1){
         next
       } 
       
       alt <- d + graph[nx,ny]
-      key <- c(nx, ny, ndir, cs)
+      key <- c(nx, ny, nbr[4], nbr[3])
       
       if(alt < mindist$get(key, Inf)){
         mindist$set(key, alt)
@@ -96,12 +81,14 @@ dijkstra <- function(graph, part2){
     }      
   }
   
+  # get all dists for reaching end goal given the conditions
   keys <- mindist$keys()
   if(!part2){
     end_goal_keys <- vapply(keys, \(x) x[1] == goal[1] && x[2] == goal[2], TRUE)
   } else {
     end_goal_keys <- vapply(keys, \(x) x[1] == goal[1] && x[2] == goal[2] && x[4] >= 4, TRUE)
   }
+  # for all ways of reaching end, find min dist
   end_goal_dists <- vapply(keys[end_goal_keys], \(k) mindist$get(k, Inf), 2)
   
   min(end_goal_dists)
@@ -112,6 +99,4 @@ dijkstra(input, part2 = FALSE)
 
 
 # Part 2
-
 dijkstra(input, part2 = TRUE)
-
