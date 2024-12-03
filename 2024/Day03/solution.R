493f95fbe831992fa4b6d267f04e6b0c17248c49
AdroMine <adro.mine@outlook.com>
2024-12-03 07:09:45 +0000

    2024 Day 03, cleaner

--- 2024/Day03/solution.R
+++ 2024/Day03/solution.R
@@ -1,33 +1,68 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 input <- readLines('input.txt')
+# Part 1 and 2 together
+instructions <- stringr::str_extract_all(input,
+                                         "mul\\(\\d+,\\d+\\)|do\\(.*?\\)|don't\\(.*?\\)") |> 
+  unlist()
 
 p1 <- 0
 p2 <- 0
-
-safe <- function(line){
-  diffs <- diff(line)
-  inc_dec <- all(diffs > 0) || all(diffs < 0)
-  lev <- all(abs(diffs) %in% c(1, 2, 3))
-  inc_dec && lev
-}
-
-for(line in input){
-  line <- as.integer(strsplit(line, " ")[[1]])
+enabled <- TRUE
+for(i in seq_along(instructions)) {
   
-  if(safe(line)) {
-    p1 <- p1 + 1
+  if(grepl("do\\(", instructions[i])){
+    enabled <- TRUE
+  } else if (grepl("don't", instructions[i])) {
+    enabled <- FALSE
   } else {
-    
-    for(i in seq_along(line)){
-      
-      if(safe(line[-i])){
-        p2 <- p2 + 1
-        break
-      }
-    }
+    nums <- stringr::str_extract_all(instructions[i], "\\d+")[[1]] |> as.integer()
+    p1 <- p1 + prod(nums)
+    p2 <- p2 + prod(nums) * enabled
   }
 }
 
 p1
-p1 + p2
+p2
+
+
+
+
+
+
+# Separately
+instructions <- stringr::str_extract_all(input, "mul\\(\\d+,\\d+\\)") |> unlist()
+
+# Part 1
+stringr::str_extract_all(instructions, "\\d+") |> 
+  lapply(as.integer) |> 
+  sapply(prod) |> 
+  sum()
+
+
+# Part 2
+instructions2 <- stringr::str_extract_all(input, "mul\\(\\d+,\\d+\\)|do\\(.*?\\)|don't\\(.*?\\)") |> 
+  unlist()
+
+enabled <- TRUE
+for(i in seq_along(instructions2)){
+  
+  if(grepl("^do\\(\\)", instructions2[i])){
+    enabled <- TRUE
+    instructions2[i] <- ""
+  } else if (grepl("don't", instructions2[i])){
+    enabled <- FALSE
+    instructions2[i] <- ""
+  }
+  
+  if(!enabled){
+    instructions2[i] <- ""
+  } 
+}
+
+instructions2[instructions2 != ""] |> 
+stringr::str_extract_all("\\d+") |> 
+  lapply(as.integer) |> 
+  sapply(prod) |> 
+  sum()
+
