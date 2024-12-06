59f70195069528ecd4f7df5bd972367d2b7e1e26
AdroMine <adro.mine@outlook.com>
2024-12-06 18:57:10 +0000

    2024 day 6 refactor

--- 2024/Day06/solution.R
+++ 2024/Day06/solution.R
@@ -1,18 +1,14 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # input <- readr::read_file('example.txt') |> 
-file_name <- 'example.txt'
+# file_name <- 'example.txt'
 file_name <- 'input.txt'
 input <- readLines(file_name)
 C <- nchar(input[1])
 R <- length(input)
-input <- read.fwf(file_name, widths = rep(1,C), comment.char = "")
-
-visited <- matrix(FALSE, nrow = length(input), ncol = C)
+input <- read.fwf(file_name, widths = rep(1,C), comment.char = "") |> as.matrix()
 
 start <- which(input == '^', arr.ind = TRUE)[1,]
-
-escaped <- FALSE
 
 dirs <- list(
   c(-1, 0), # up
@@ -20,47 +16,18 @@ dirs <- list(
   c(1, 0),  # down
   c(0, -1)  # left
 )
-cur_dir <- 1
-cur_pos <- start
-visited[cur_pos[1], cur_pos[2]] <- TRUE
 
-while(!escaped){
+maze_traverse <- function(grid, cur_pos, stone_location = NULL){
   
-  new_pos <- cur_pos + dirs[[cur_dir]]
-  
-  if(new_pos[2] > C | new_pos[2] < 1 | new_pos[1] > R | new_pos[1] < 1){
-    escaped <- TRUE
-    next
+  if(!is.null(stone_location)){
+    grid[stone_location[1], stone_location[2]] <- '#'
   }
   
-  if(input[new_pos[1], new_pos[2]] == '#'){
-    # change dir
-    cur_dir <- (cur_dir - 1 + 1) %% 4 + 1
-  } else {
-    visited[new_pos[1], new_pos[2]] <- TRUE
-    cur_pos <- new_pos
-  }
-}
-
-sum(visited)
-
-# Part 2
-
-
-
-possible_locations <- which(input == '.', arr.ind = TRUE)
-
-loop_result <- function(stone_location, grid){
-  
-  grid[stone_location[1], stone_location[2]] <- '#'
-  start <- which(input == '^', arr.ind = TRUE)[1,]
   # 0 means not visited, 1-4 means visited while facing particular direction
   # if visit same spot with same direction, means loop
-  visited <- matrix(0, nrow = length(input), ncol = C)
+  visited <- matrix(0, nrow = nrow(grid), ncol = C)
   cur_dir <- 1
-  cur_pos <- start
   visited[cur_pos[1], cur_pos[2]] <- cur_dir
-  
   escaped <- FALSE
   while(!escaped){
     
@@ -70,7 +37,7 @@ loop_result <- function(stone_location, grid){
       next
     }
     
-    if(input[new_pos[1], new_pos[2]] == '#'){
+    if(grid[new_pos[1], new_pos[2]] == '#'){
       # change dir
       cur_dir <- (cur_dir - 1 + 1) %% 4 + 1
     } else {
@@ -84,15 +51,26 @@ loop_result <- function(stone_location, grid){
     }
   }
   
-  return(!escaped)
-  
-  
+  if(escaped & is.null(stone_location)){
+    visited
+  } else {
+    !escaped
+  }
 }
 
+p1_visited <- maze_traverse(input, start)
+
+# Part 1
+sum(p1_visited != 0)
+
+# Part 2
+
+possible_locations <- which(p1_visited != 0, arr.ind = TRUE)
+
 p2 <- 0
-for(i in 1:nrow(possible_locations)){
+for(i in cli::cli_progress_along(1:nrow(possible_locations))) {
   new_location <- possible_locations[i,]
-  res <- loop_result(new_location, input)
+  res <- maze_traverse(input, start, new_location)
   p2 <- p2 + res 
   start <- which(input == '^', arr.ind = TRUE)[1,]
 }
